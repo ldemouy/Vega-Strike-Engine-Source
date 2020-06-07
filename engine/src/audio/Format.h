a1c04fcd5c9931e051bce4b303a85cfa3309b46e
Luke De Mouy <lukedemouy@gmail.com>
2020-06-07 15:11:42 -0700

    Change to fixed width types

--- engine/src/audio/Format.h
+++ engine/src/audio/Format.h
@@ -3,56 +3,53 @@
 //
 #ifndef __AUDIO_FORMAT_H__INCLUDED__
 #define __AUDIO_FORMAT_H__INCLUDED__
-
-namespace Audio {
+#include <stdint.h>
+namespace Audio
+{
 
     /**
      * Audio format information class
      *
      */
-    struct Format {
-        unsigned int sampleFrequency;
-        unsigned char bitsPerSample;
-        unsigned char channels;
-        int signedSamples : 1;
-        int nativeOrder : 1;
-        
+    struct Format
+    {
+        uint32_t sampleFrequency;
+        uint8_t bitsPerSample;
+        uint8_t channels;
+        int32_t signedSamples : 1;
+        int32_t nativeOrder : 1;
+
         Format() {}
-        
-        Format(unsigned int freq, unsigned char bps, unsigned char nch) :
-            sampleFrequency(freq),
-            bitsPerSample(bps),
-            channels(nch),
-            signedSamples((bps>=16)?1:0),
-            nativeOrder(1)
+
+        Format(uint32_t freq, uint8_t bps, uint8_t nch) : sampleFrequency(freq),
+                                                          bitsPerSample(bps),
+                                                          channels(nch),
+                                                          signedSamples((bps >= 16) ? 1 : 0),
+                                                          nativeOrder(1)
         {
         }
-        
-        unsigned int frameSize() const
+
+        uint32_t frameSize() const
         {
             return (bitsPerSample * channels + 7) / 8;
         }
-        
-        unsigned int bytesPerSecond() const
+
+        uint32_t bytesPerSecond() const
         {
             return frameSize() * sampleFrequency;
         }
-        
+
         bool operator==(const Format &o) const
         {
-            return (sampleFrequency == o.sampleFrequency)
-                && (bitsPerSample == o.bitsPerSample)
-                && (channels == o.channels)
-                && (signedSamples == o.signedSamples)
-                && (nativeOrder == o.nativeOrder);
+            return (sampleFrequency == o.sampleFrequency) && (bitsPerSample == o.bitsPerSample) && (channels == o.channels) && (signedSamples == o.signedSamples) && (nativeOrder == o.nativeOrder);
         }
-        
+
         bool operator!=(const Format &o) const
         {
             return !(*this == o);
         }
     };
 
-};
+}; // namespace Audio
 
-#endif//__AUDIO_FORMAT_H__INCLUDED__
+#endif //__AUDIO_FORMAT_H__INCLUDED__
