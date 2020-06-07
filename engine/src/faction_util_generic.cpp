6733a5180859581b0bf4b47d3ba824ca4d97c271
Luke De Mouy <lukedemouy@gmail.com>
2020-06-07 15:13:00 -0700

    Refactoring on faction files includes: * replace boost shared ptr with std ones * reformat code * use fixed width types * fix braces

--- engine/src/faction_util_generic.cpp
+++ engine/src/faction_util_generic.cpp
@@ -9,37 +9,35 @@
 
 #include "options.h"
 
-
-
 using namespace FactionUtil;
-int FactionUtil:: upgradefac = 0;
-int FactionUtil:: planetfac  = 0;
-int FactionUtil:: neutralfac = 0;
-FSM* FactionUtil::GetConversation( int Myfaction, int TheirFaction )
+int32_t FactionUtil::upgradefac = 0;
+int32_t FactionUtil::planetfac = 0;
+int32_t FactionUtil::neutralfac = 0;
+FSM *FactionUtil::GetConversation(const int32_t &Myfaction, const int32_t &TheirFaction)
 {
-    assert( factions[Myfaction]->faction[TheirFaction].stats.index == TheirFaction );
+    assert(factions[Myfaction]->faction[TheirFaction].stats.index == TheirFaction);
     return factions[Myfaction]->faction[TheirFaction].conversation.get();
 }
 
-const char* FactionUtil::GetFaction( int i )
+const char *FactionUtil::GetFaction(int32_t i)
 {
-    if ( i >= 0 && i < (int) factions.size() )
+    if (i >= 0 && i < (int)factions.size())
         return factions[i]->factionname;
     return "";
 }
 
-static int GetFactionLookup( const char *factionname )
+static int32_t GetFactionLookup(const char *factionname)
 {
 #ifdef _MSC_VER
-  #define strcasecmp stricmp
+#define strcasecmp stricmp
 #endif
-    for (unsigned int i = 0; i < factions.size(); i++)
-        if (strcasecmp( factionname, factions[i]->factionname ) == 0)
+    for (size_t i = 0; i < factions.size(); i++)
+        if (strcasecmp(factionname, factions[i]->factionname) == 0)
             return i;
     return 0;
 }
 
-Unit* FactionUtil::GetContraband( int faction )
+Unit *FactionUtil::GetContraband(int32_t faction)
 {
     return factions[faction]->contraband.get();
 }
@@ -48,62 +46,70 @@ Unit* FactionUtil::GetContraband( int faction )
  * 1 is happy. 0 is neutral (btw 1 and 0 will not attack)
  * -1 is mad. <0 will attack
  */
-int FactionUtil::GetFactionIndex( const string& name )
+int32_t FactionUtil::GetFactionIndex(const string &name)
 {
-    static Hashtable< string, int, 47 >factioncache;
-    int *tmp = factioncache.Get( name );
+    static Hashtable<string, int32_t, 47> factioncache;
+    int32_t *tmp = factioncache.Get(name);
     if (tmp)
         return *tmp;
-    int  i   = GetFactionLookup( name.c_str() );
-    tmp  = new int;
+    int32_t i = GetFactionLookup(name.c_str());
+    tmp = new int32_t;
     *tmp = i;
-    factioncache.Put( name, tmp );
+    factioncache.Put(name, tmp);
     return i;
 }
-bool FactionUtil::isCitizenInt( int faction )
+bool FactionUtil::isCitizenInt(int32_t faction)
 {
     return factions[faction]->citizen;
 }
-bool FactionUtil::isCitizen( const std::string& name )
+bool FactionUtil::isCitizen(const std::string &name)
 {
-    return isCitizenInt( GetFactionIndex( name ) );
+    return isCitizenInt(GetFactionIndex(name));
 }
-string FactionUtil::GetFactionName( int index )
+string FactionUtil::GetFactionName(int32_t index)
 {
-    const char *tmp = GetFaction( index );
-    if (tmp) return tmp;
+    const char *tmp = GetFaction(index);
+    if (tmp)
+        return tmp;
     static std::string nullstr;
     return nullstr;
 }
 
-static bool isPlayerFaction( const int MyFaction )
+static bool isPlayerFaction(const int32_t &MyFaction)
 {
-    unsigned int numplayers = _Universe->numPlayers();
-    for (unsigned int i = 0; i < numplayers; ++i) {
-        Unit *un = _Universe->AccessCockpit( i )->GetParent();
+    uint32_t numplayers = _Universe->numPlayers();
+    for (size_t i = 0; i < numplayers; ++i)
+    {
+        Unit *un = _Universe->AccessCockpit(i)->GetParent();
         if (un)
             if (un->faction == MyFaction)
                 return true;
     }
     return false;
 }
-void FactionUtil::AdjustIntRelation( const int Myfaction, const int TheirFaction, float factor, float rank )
+void FactionUtil::AdjustIntRelation(const int &Myfaction, const int &TheirFaction, float factor, float rank)
 {
-    assert( factions[Myfaction]->faction[TheirFaction].stats.index == TheirFaction );
-    if (strcmp( factions[Myfaction]->factionname, "neutral" ) != 0) {
-        if (strcmp( factions[Myfaction]->factionname, "upgrades" ) != 0) {
-            if (strcmp( factions[TheirFaction]->factionname, "neutral" ) != 0) {
-                if (strcmp( factions[TheirFaction]->factionname, "upgrades" ) != 0) {
-                    if (isPlayerFaction( TheirFaction ) || game_options.AllowNonplayerFactionChange) {
-                        if (game_options.AllowCivilWar || Myfaction != TheirFaction) {
-                            factions[Myfaction]->faction[TheirFaction].relationship += factor*rank;
+    assert(factions[Myfaction]->faction[TheirFaction].stats.index == TheirFaction);
+    if (strcmp(factions[Myfaction]->factionname, "neutral") != 0)
+    {
+        if (strcmp(factions[Myfaction]->factionname, "upgrades") != 0)
+        {
+            if (strcmp(factions[TheirFaction]->factionname, "neutral") != 0)
+            {
+                if (strcmp(factions[TheirFaction]->factionname, "upgrades") != 0)
+                {
+                    if (isPlayerFaction(TheirFaction) || game_options.AllowNonplayerFactionChange)
+                    {
+                        if (game_options.AllowCivilWar || Myfaction != TheirFaction)
+                        {
+                            factions[Myfaction]->faction[TheirFaction].relationship += factor * rank;
                             if (factions[Myfaction]->faction[TheirFaction].relationship > 1 && game_options.CappedFactionRating)
                                 factions[Myfaction]->faction[TheirFaction].relationship = 1;
                             if (factions[Myfaction]->faction[TheirFaction].relationship < game_options.min_relationship)
                                 factions[Myfaction]->faction[TheirFaction].relationship = game_options.min_relationship;
                             if (!game_options.AllowNonplayerFactionChange)
                                 factions[TheirFaction]->faction[Myfaction].relationship =
-                                    factions[Myfaction]->faction[TheirFaction].relationship;                                                                                     //reflect if player
+                                    factions[Myfaction]->faction[TheirFaction].relationship; //reflect if player
                         }
                     }
                 }
@@ -111,67 +117,80 @@ void FactionUtil::AdjustIntRelation( const int Myfaction, const int TheirFaction
         }
     }
 }
-int FactionUtil::GetPlaylist( const int myfaction )
+int32_t FactionUtil::GetPlaylist(const int &myfaction)
 {
-    return factions[myfaction]->playlist;     //can be -1
+    return factions[myfaction]->playlist; //can be -1
 }
-const float* FactionUtil::GetSparkColor( const int myfaction )
+const float *FactionUtil::GetSparkColor(const int &myfaction)
 {
-    return factions[myfaction]->sparkcolor;     //can be -1
+    return factions[myfaction]->sparkcolor; //can be -1
 }
-unsigned int FactionUtil::GetNumFactions()
+uint32_t FactionUtil::GetNumFactions()
 {
     return factions.size();
 }
-void FactionUtil::SerializeFaction( FILE *fp )
+void FactionUtil::SerializeFaction(FILE *fp)
 {
-    for (unsigned int i = 0; i < factions.size(); i++) {
-        for (unsigned int j = 0; j < factions[i]->faction.size(); j++)
-            VSFileSystem::vs_fprintf( fp, "%g ", factions[i]->faction[j].relationship );
-        VSFileSystem::vs_fprintf( fp, "\n" );
+    for (size_t i = 0; i < factions.size(); i++)
+    {
+        for (size_t j = 0; j < factions[i]->faction.size(); j++)
+        {
+            VSFileSystem::vs_fprintf(fp, "%g ", factions[i]->faction[j].relationship);
+        }
+        VSFileSystem::vs_fprintf(fp, "\n");
     }
 }
 string FactionUtil::SerializeFaction()
 {
-    char   temp[8192];
-    string ret( "" );
-    for (unsigned int i = 0; i < factions.size(); i++) {
-        for (unsigned int j = 0; j < factions[i]->faction.size(); j++) {
-            sprintf( temp, "%g ", factions[i]->faction[j].relationship );
-            ret += string( temp );
+    char temp[8192];
+    string ret("");
+    for (unsigned int i = 0; i < factions.size(); i++)
+    {
+        for (unsigned int j = 0; j < factions[i]->faction.size(); j++)
+        {
+            sprintf(temp, "%g ", factions[i]->faction[j].relationship);
+            ret += string(temp);
         }
-        sprintf( temp, "\n" );
-        ret += string( temp );
+        sprintf(temp, "\n");
+        ret += string(temp);
     }
     return ret;
 }
-int FactionUtil::numnums( const char *str )
+int FactionUtil::numnums(const char *str)
 {
     int count = 0;
     for (int i = 0; str[i]; i++)
         count += (str[i] >= '0' && str[i] <= '9') ? 1 : 0;
     return count;
 }
-void FactionUtil::LoadSerializedFaction( FILE *fp )
+void FactionUtil::LoadSerializedFaction(FILE *fp)
 {
-    for (unsigned int i = 0; i < factions.size(); i++) {
-        char *tmp  = new char[24*factions[i]->faction.size()];
-        fgets( tmp, 24*factions[i]->faction.size()-1, fp );
+    for (size_t i = 0; i < factions.size(); i++)
+    {
+        char *tmp = new char[24 * factions[i]->faction.size()];
+        fgets(tmp, 24 * factions[i]->faction.size() - 1, fp);
         char *tmp2 = tmp;
-        if (numnums( tmp ) == 0) {
+        if (numnums(tmp) == 0)
+        {
             i--;
             continue;
         }
-        for (unsigned int j = 0; j < factions[i]->faction.size(); j++) {
-            if ( 1 != sscanf( tmp2, "%f ", &factions[i]->faction[j].relationship ) )
-                printf( "err" );
-            int  k = 0;
+        for (size_t j = 0; j < factions[i]->faction.size(); j++)
+        {
+            if (1 != sscanf(tmp2, "%f ", &factions[i]->faction[j].relationship))
+                printf("err");
+            int32_t k = 0;
             bool founddig = false;
-            while (tmp2[k]) {
-                if ( isdigit( tmp2[k] ) )
+            while (tmp2[k])
+            {
+                if (isdigit(tmp2[k]))
+                {
                     founddig = true;
-                if ( founddig && (!isdigit( tmp2[k] ) && tmp2[k] != '.') )
+                }
+                if (founddig && (!isdigit(tmp2[k]) && tmp2[k] != '.'))
+                {
                     break;
+                }
                 k++;
             }
             tmp2 += k;
@@ -181,48 +200,63 @@ void FactionUtil::LoadSerializedFaction( FILE *fp )
         delete[] tmp;
     }
 }
-bool whitespaceNewline( char *inp )
+bool whitespaceNewline(char *inp)
 {
-    for (; *inp; inp++) {
+    for (; *inp; inp++)
+    {
         if (inp[0] == '\n' || inp[0] == '\r')
+        {
             return true;
+        }
         if (inp[0] != ' ' && inp[0] != '\t')
+        {
             break;
+        }
     }
     return false;
 }
 string savedFactions;
-void FactionUtil::LoadSerializedFaction( char* &buf )
+void FactionUtil::LoadSerializedFaction(char *&buf)
 {
-    if (buf == NULL) {
-        char *bleh = strdup( savedFactions.c_str() );
+    if (buf == NULL)
+    {
+        char *bleh = strdup(savedFactions.c_str());
         char *blah = bleh;
-        LoadSerializedFaction( blah );
-        free( bleh );
+        LoadSerializedFaction(blah);
+        free(bleh);
         return;
     }
-    if (factions.size() == 0) {
+    if (factions.size() == 0)
+    {
         savedFactions = buf;
         return;
     }
-    for (unsigned int i = 0; i < factions.size(); i++) {
-        if (numnums( buf ) == 0)
+    for (size_t i = 0; i < factions.size(); i++)
+    {
+        if (numnums(buf) == 0)
+        {
             return;
-        for (unsigned int j = 0; j < factions[i]->faction.size(); j++) {
-            sscanf( buf, "%f ", &factions[i]->faction[j].relationship );
-            int  k = 0;
+        }
+        for (unsigned int j = 0; j < factions[i]->faction.size(); j++)
+        {
+            sscanf(buf, "%f ", &factions[i]->faction[j].relationship);
+            int32_t k = 0;
             bool founddig = false;
-            while (buf[k]) {
-                if ( isdigit( buf[k] ) )
+            while (buf[k])
+            {
+                if (isdigit(buf[k]))
+                {
                     founddig = true;
-                if ( founddig && (!isdigit( buf[k] ) && buf[k] != '.') )
+                }
+                if (founddig && (!isdigit(buf[k]) && buf[k] != '.'))
+                {
                     break;
+                }
                 k++;
             }
             buf += k;
-            if ( whitespaceNewline( buf ) )
+            if (whitespaceNewline(buf))
                 break;
         }
     }
 }
-
--- engine/src/faction_util_server.cpp
+++ engine/src/faction_util_server.cpp
@@ -5,43 +5,44 @@ Faction::~Faction()
 {
     delete[] factionname;
     if (contraband.get())
+    {
         contraband->Kill();
+    }
 }
 
-std::vector< Animation* >* FactionUtil::GetRandCommAnimation( int faction, Unit*, unsigned char &sex )
-{
-    return NULL;
-}
-
-std::vector< Animation* >* FactionUtil::GetAnimation( int faction, int n, unsigned char &sex )
+std::vector<Animation *> *FactionUtil::GetRandCommAnimation(int32_t faction, Unit *, uint8_t &sex)
 {
     return NULL;
 }
 
-Animation* FactionUtil::createAnimation( const char *anim )
+std::vector<Animation *> *FactionUtil::GetAnimation(int32_t faction, int32_t n, uint8_t &sex)
 {
     return NULL;
 }
-Texture* FactionUtil::createTexture( const char *tex, const char *tmp, bool force )
+
+Animation *FactionUtil::createAnimation(const char *anim)
 {
     return NULL;
 }
-Texture* FactionUtil::createTexture( const char *tex, bool force )
+Texture *FactionUtil::createTexture(const char *tex, const char *tmp, bool force)
+{
+    return NULL;
+}
+Texture *FactionUtil::createTexture(const char *tex, bool force)
 {
     return NULL;
 }
 void FactionUtil::LoadFactionPlaylists() {}
 
-Texture* FactionUtil::getForceLogo( int faction )
+Texture *FactionUtil::getForceLogo(int32_t faction)
 {
     return NULL;
 }
-Texture* FactionUtil::getSquadLogo( int faction )
+Texture *FactionUtil::getSquadLogo(int32_t faction)
 {
     return NULL;
 }
-int FactionUtil::GetNumAnimation( int faction )
+int32_t FactionUtil::GetNumAnimation(int32_t faction)
 {
     return 0;
 }
-
