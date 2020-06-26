b2510c2b134f0eb5d84c9a78ec91189b32da6cf6
Luke De Mouy <lukedemouy@gmail.com>
2020-06-25 20:01:30 -0700

    remove unused typedefs in prep for removal of ones that are actually used

--- engine/src/cmd/collide2/Ice/IceTypes.h
+++ engine/src/cmd/collide2/Ice/IceTypes.h
@@ -29,16 +29,11 @@
 #define INVSQRT3 0.577350269189f	 //!< 1 / sqrt(3)
 
 // Custom types used in ICE
-typedef signed char sbyte;	  //!< sizeof(sbyte)	must be 1
 typedef unsigned char ubyte;  //!< sizeof(ubyte)	must be 1
 typedef signed short sword;	  //!< sizeof(sword)	must be 2
 typedef unsigned short uword; //!< sizeof(uword)	must be 2
 typedef signed int sdword;	  //!< sizeof(sdword)	must be 4
 typedef unsigned int udword;  //!< sizeof(udword)	must be 4
-typedef int64_t sqword;		  //!< sizeof(sqword)	must be 8
-typedef uint64_t uqword;	  //!< sizeof(uqword)	must be 8
-typedef float float32;		  //!< sizeof(float32)	must be 4
-typedef double float64;		  //!< sizeof(float64)	must be 4
 
 #define INVALID_ID 0xffffffff //!< Invalid dword ID (counterpart of null pointers)
 
