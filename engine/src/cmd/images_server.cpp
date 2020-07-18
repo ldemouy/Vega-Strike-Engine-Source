00a547d9af9512e5505f9a4616a2d39ce57338dd
Luke De Mouy <lukedemouy@gmail.com>
2020-07-18 16:57:13 -0700

    Remove unnecessary code

--- engine/src/cmd/images_server.cpp
+++ engine/src/cmd/images_server.cpp
@@ -3,10 +3,8 @@
 template <typename BOGUS> UnitImages<BOGUS>::~UnitImages()
 {
     if (pExplosion)
+    {
         delete pExplosion;
+    }
     VSDESTRUCT1
 }
-
-// explicit instantiation needed
-template struct UnitImages<void>;
-static UnitImages<void> bleh;
